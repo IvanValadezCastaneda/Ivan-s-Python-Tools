6889701012c5fd09a017386b1c483ad122ddaeae
Ivan Valadez Castañeda <ivanvaladezcastaneda@gmail.com>
2022-12-23 17:17:19 -0600

    Update ChangeBackground.py

--- ChangeBackground.py
+++ ChangeBackground.py
@@ -4,44 +4,45 @@ import os
 DETAILEDFILES = []
 # The directory where your .xml files are located
 # You can change this to any directory you want
-directory = input("Enter directory")
+directory = input("Enter directory: ")
 
 # Get a list of all .xml files in the directory
 xml_files = [f for f in os.listdir(directory) if f.endswith(".xml")]
 
-# Print the names of all .xml files that end with  desired word "desiredword.xml"
+# Print the names of all .xml files that end with "DETAILED.xml"
 for xml_file in xml_files:
-  if xml_file.endswith(".xml"):
+  if xml_file.endswith("SYM.xml"):
     print(xml_file)
     DETAILEDFILES.append(xml_file)
 #print (DETAILEDFILES)
-
-# The name of the .xml file that you want to update
-xml_file = input(" Ingresa el archivo ")
 
 # The properties that you want to add to your .xml file
 properties = """
 <properties>
   <prop name="Name">
+   <prop name="es_MX.utf8" />
   </prop>
-  <prop name="Size"></prop>
-  <prop name="BackColor"></prop>
-  <prop name="RefFileName" />
-  <prop name="layoutType">None</prop>
+  <prop name="Size">230 100</prop>
+  <prop name="BackColor">ISA_11_BackgroundGrey</prop>
  </properties>
 """
 
-# Parse the .xml file
-tree = ET.parse(xml_file)
-root = tree.getroot()
-
-# Find the <properties> element
-properties_elem = root.find("properties")
-
-# If there is a <properties> element, replace it with the new one
-if properties_elem is not None:
-  root.remove(properties_elem)
-  root.append(ET.fromstring(properties))
-
-# Write the updated XML to the file
-tree.write(xml_file)
+# Loop through all the XML files in the DETAILEDFILES list
+for xml_file in DETAILEDFILES:
+  # Construct the full file path
+  file_path = os.path.join(directory, xml_file)
+  
+  # Parse the .xml file
+  tree = ET.parse(file_path)
+  root = tree.getroot()
+  
+  # Find the <properties> element
+  properties_elem = root.find("properties")
+  
+  # If there is a <properties> element, replace it with the new one
+  if properties_elem is not None:
+    root.remove(properties_elem)
+    root.append(ET.fromstring(properties))
+  
+  # Write the updated XML to the file
+  tree.write(file_path)
