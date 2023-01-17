6d5761e989e1d044ddcc517f7ded41ecf758870e
Ivan Valadez Castaneda <ivanvaladezcastaneda@gmail.com>
2023-01-17 09:56:00 -0600

    Replace Objects Name in Blender Script

---
+++ Replace Object Name in Blender.py
@@ -0,0 +1,7 @@
+import bpy
+
+def replace_string_in_object_names(input_string, replacement_string):
+    for obj in bpy.data.objects:
+        obj.name = obj.name.replace(input_string, replacement_string)
+
+replace_string_in_object_names("/", "-")
